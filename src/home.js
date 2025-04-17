30dea061ce08a47d9b20eb25a342342a4b0b75f2
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-04-17 07:05:28 +0300

    begin

--- src/home.js
+++ src/home.js
@@ -1,1 +1,17 @@
 //Логіка сторінки Home
+
+import { getCategories, getProducts } from './js/products-api';
+
+const loadPage = async () => {
+  try {
+    const categories = await getCategories();
+    console.log(categories);
+    const { products } = await getProducts();
+    console.log(products);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// loadPage();
+window.addEventListener(`DOMContentLoaded`, loadPage);
--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,18 @@
 // Функції для роботи з бекендом
+
+import axios from 'axios';
+
+export const getCategories = async () => {
+  const { data } = await axios.get(
+    'https://dummyjson.com/products/category-list'
+  );
+  return data;
+};
+
+export const getProducts = async (page = 1) => {
+  const skip = (page - 1) * 12;
+  const { data } = await axios.get(
+    `https://dummyjson.com/products?limit=12&skip=${skip}`
+  );
+  return data;
+};
