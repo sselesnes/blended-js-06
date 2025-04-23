bdf24a9a46a85c4449bbf445624e529036fd34b5
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-04-23 07:38:27 +0300

    search from cart

--- src/cart.js
+++ src/cart.js
@@ -1,4 +1,14 @@
 //Логіка сторінки Cart
+import { LS } from './js/storage';
+import { refs } from './js/refs';
+import { handlers } from './js/handlers';
+import { getProduct } from './js/products-api';
 import { updateHeader } from './js/render-function';
 
-window.addEventListener(`DOMContentLoaded`, updateHeader);
+function initCart() {
+  updateHeader();
+}
+
+refs.searchForm.addEventListener('submit', handlers.searchProduct);
+refs.searchForm.addEventListener('click', handlers.searchClear);
+window.addEventListener(`DOMContentLoaded`, initCart);
